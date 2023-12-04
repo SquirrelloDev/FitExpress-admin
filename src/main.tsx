3f38af44854bb0c2597e3c2524ea5350632a9517
SquirrelloDev <olejnike2000@gmail.com>
2023-12-04 18:15:33 +0100

    Add styles, remove css init file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './sass/main.scss'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import UnauthorizedLayout from "./layouts/UnauthorizedLayout";
 import {NotFound} from "./pages/NotFound";
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,15 @@
+import classes from "../sass/pages/login.module.scss";
+import btnStyles from '../sass/components/button.module.scss'
+import inputStyles from '../sass/components/text-input.module.scss'
 export function Login() {
 	return (
-		<div>
-			<p>login page</p>
-		</div>
+		<>
+			<h1 className={classes.header__heading}>FitExpress</h1>
+			<form className={classes.form}>
+				<input type='text' className={inputStyles.input} placeholder={'Adres e-mail'}/>
+				<input type='password' className={inputStyles.input} placeholder={"Hasło"}/>
+				<button type='submit' className={`${classes.form__submit} ${btnStyles.btn}`}>Zaloguj</button>
+			</form>
+		</>
 	)
 }

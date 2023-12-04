28d27ada7723ba1d0b847c7168406ee8bf54e7de
SquirrelloDev <olejnike2000@gmail.com>
2023-12-04 15:18:12 +0100

    Add first get request

--- src/types/UserData.ts
+++ src/types/UserData.ts
@@ -1,8 +1,21 @@
 import {UserRole} from "../utils/userRoles";
+import {Address} from "./Address";
+import {Promocode} from "./Promocode";
+import {HealthData} from "./HealthData";
 
 export type UserData = {
     id: string,
     name: string,
     role: UserRole,
     token: string
+}
+export type UserFullData = {
+    _id: string,
+    name: string,
+    role: UserRole,
+    resetToken: string,
+    birthDate: Date,
+    addresses: Address[],
+    redeemedCodes: Promocode[],
+    healthData: HealthData
 }

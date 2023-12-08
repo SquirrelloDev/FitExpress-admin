d4b491873d1f350e3ff28594898d8f9315069ba8
SquirrelloDev <olejnike2000@gmail.com>
2023-12-08 20:38:42 +0100

    Add styles for the deleteview

---
+++ src/components/Modal/Views/UserDelete.tsx
@@ -0,0 +1,29 @@
+import {IconX} from "@tabler/icons-react";
+import classes from "../../../sass/components/deleteModalView.module.scss";
+
+interface ViewDeleteProps {
+	id: string
+	closeModal: (data: {isOpen: boolean, deleteId: string}) => void
+	deleteMutation: () => void
+}
+function UserDelete({id, closeModal, deleteMutation}:ViewDeleteProps) {
+	const deleteFn = () => {
+		deleteMutation()
+	}
+	return (
+		<div className={classes.view}>
+			<div className={classes.view__title}>
+				<h2>Usunąć?</h2>
+				<button onClick={() => closeModal({isOpen: false, deleteId: ""})}><IconX /></button>
+			</div>
+			<div className={classes.view__info}>
+				<p>Zamierzasz usunąć wpis o identyfikatorze {id}. Jego dane zostaną usunięte na zawsze! Jesteś tego pewien?</p>
+			</div>
+				<div className={classes.view__buttons}>
+					<button onClick={deleteFn}>Usuń</button>
+					<button onClick={() => closeModal({isOpen: false, deleteId: ""})}>Anuluj</button>
+				</div>
+		</div>
+	)
+}
+export default UserDelete

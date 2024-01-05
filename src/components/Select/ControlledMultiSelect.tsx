53fee5906b14156821ab06898ebd2964af324f6a
SquirrelloDev <olejnike2000@gmail.com>
2024-01-05 20:48:51 +0100

    Add multiselect component

---
+++ src/components/Select/ControlledMultiSelect.tsx
@@ -0,0 +1,96 @@
+import clsx from 'clsx'
+import { Control, Controller, useFormContext } from 'react-hook-form'
+import Select, {
+    Props as SelectProps,
+} from 'react-select'
+import { SelectOption } from './types'
+import classes from "../../sass/components/select.module.scss";
+
+interface ControlledSelectProps extends Omit<SelectProps, 'options'> {
+  options: SelectOption[]
+  control: Control
+  name: string
+  error?: string
+  isRequired?: boolean
+}
+function ControlledSelect({
+  options,
+  control,
+  name = '',
+  placeholder = '',
+  isDisabled,
+  isRequired = false,
+  ...props
+}: ControlledSelectProps) {
+  const {
+    formState: { errors },
+  } = useFormContext()
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({
+        field: { onChange, onBlur, value },
+        fieldState: { isTouched },
+      }) => (
+        <div>
+            <label className={classes.select__label}>{placeholder}</label>
+          <Select
+            options={options}
+            value={options.find((option) => option.value === value)}
+            onBlur={onBlur}
+            //@ts-expect-error value is correct
+            onChange={
+              (vals: SelectOption[]) => {
+              if (!vals) {
+                onChange(null)
+                return
+              }
+              onChange(vals.map(val => val.value))
+            }}
+            isMulti
+            styles={{
+                control: (base) => ({
+                    ...base,
+                    backgroundColor: '#1a1a1a',
+                    borderRadius: '8px'
+                }),
+                menu: (baseStyles) => ({
+                    ...baseStyles,
+                    backgroundColor: '#252525FF'
+                }),
+                singleValue: (baseStyles) => ({
+                    ...baseStyles,
+                    color: '#fff'
+                }),
+                option: (baseStyles, state) => ({
+                    ...baseStyles,
+                    backgroundColor: state.isSelected ? '#46C367FF' : 'none'
+                })
+            }}
+            classNames={{
+                control: () => classes.select__control,
+                option: ({ isSelected }) =>
+                    clsx(
+                        classes.select__option,
+                        isSelected && classes['select__option--selected']
+                    )
+            }}
+            {...props}
+          />
+          {errors[name] && (
+            <p >
+              {`${errors[name]?.message}`}
+            </p>
+          )}
+          {!errors[name] && isTouched && !value && isRequired && (
+            <p>
+              {'required'}
+            </p>
+          )}
+        </div>
+      )}
+    />
+  )
+}
+export default ControlledSelect

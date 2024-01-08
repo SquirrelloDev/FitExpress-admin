ddd51a94cb00e6b59b0ec44984ee284dcf2ddbb2
SquirrelloDev <olejnike2000@gmail.com>
2024-01-08 19:03:58 +0100

    Add useUserOrders hook

---
+++ src/hooks/useUserOrders.ts
@@ -0,0 +1,20 @@
+import {SelectOption} from "../components/Select/types";
+import {useEffect, useState} from "react";
+import {Address} from "../types/dbtypes/Address";
+import {Order} from "../types/dbtypes/Order";
+
+function useUserOrders(clientId: string, allOrders: Order[]): SelectOption[] {
+    const [selectedUserOrders, setSelectedUserOrders] = useState<SelectOption[]>([])
+    useEffect(() => {
+        const filteredOrders = allOrders.filter(order => order.user_id._id === clientId)
+        const mappedAddresses: SelectOption[] = filteredOrders.map(filteredOrder => {
+            return {
+                label: filteredOrder.name,
+                value: filteredOrder._id
+            }
+        })
+        setSelectedUserOrders(mappedAddresses);
+    }, [allOrders, clientId])
+    return selectedUserOrders
+}
+export default useUserOrders

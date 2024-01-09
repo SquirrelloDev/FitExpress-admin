4d13beb2f0a6acfd1a8e93237e3038ce835be0bc
SquirrelloDev <olejnike2000@gmail.com>
2024-01-09 17:00:22 +0100

    Add default values for Fixed edit form

--- src/pages/Fixed/FixedEdit.tsx
+++ src/pages/Fixed/FixedEdit.tsx
@@ -19,14 +19,23 @@ interface FixedEditProps {
 	id: string
 }
 function FixedEdit({data,token,id}:FixedEditProps) {
+	const {mutate, isLoading: isFormLoading} = useDayFixedEdit()
+	const {data: dietData,  isLoading: isDietsLoading} = useDietsListQuery({token: token})
+	const dietsDefaultVals: Record<string, string> = data!.diets.reduce((acc: Record<string, string>, item) => {
+		acc[`meals.${item.diet_id._id}.0`]  = item.meals.morning._id
+		acc[`meals.${item.diet_id._id}.1`]  = item.meals.lunch._id
+		acc[`meals.${item.diet_id._id}.2`]  = item.meals.dinner._id
+		acc[`meals.${item.diet_id._id}.3`]  = item.meals.teatime._id
+		acc[`meals.${item.diet_id._id}.4`]  = item.meals.supper._id
+		return acc
+	}, {})
 	const methods = useForm({
 		resolver: zodResolver(fixedSchema),
 		defaultValues: {
-			date: new Date(data.date)
+			date: new Date(data.date),
+			...dietsDefaultVals
 		}
 	})
-	const {mutate, isLoading: isFormLoading} = useDayFixedEdit()
-	const {data: dietData,  isLoading: isDietsLoading} = useDietsListQuery({token: token})
 	const selectMeals = useMealOwner()
 	const { handleSubmit } = methods
 	const onSubmit = (data: FixedSchema) => {
@@ -45,7 +54,7 @@ function FixedEdit({data,token,id}:FixedEditProps) {
 				<h2>Edytuj dzień Fixed</h2>
 				{/*@ts-expect-error data is fetched correctly*/}
 				<form className={classes.form__form} onSubmit={handleSubmit(onSubmit)}>
-					<ControlledDatePicker name={'date'} control={methods.control} placeholder='Data'/>
+					<ControlledDatePicker name={'date'} control={methods.control} placeholderText='Data'/>
 					{!isDietsLoading && (
 						<>
 							<h3 style={{margin: '15px 0'}}>Diety</h3>
@@ -53,7 +62,7 @@ function FixedEdit({data,token,id}:FixedEditProps) {
 								(
 									<div key={diet._id} className={classes['form__fixed-container__wrapper']}>
 										<h4>{diet.name}</h4>
-										<div className={classes['form__fixed-container']}>
+										<div className={clsx(classes['form__day-container--fixed'], classes['form__day-container'])}>
 											<ControlledSelect control={methods.control} options={selectMeals} name={`meals.${diet._id}.0`} placeholder={'Śniadanie'}/>
 											<ControlledSelect control={methods.control} options={selectMeals} name={`meals.${diet._id}.1`} placeholder={'Lunch'}/>
 											<ControlledSelect control={methods.control} options={selectMeals} name={`meals.${diet._id}.2`} placeholder={'Obiad'}/>

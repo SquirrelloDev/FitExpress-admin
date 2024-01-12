27333d19a23e38557ca1558eca0bb5dd3d229442
SquirrelloDev <olejnike2000@gmail.com>
2024-01-12 19:37:04 +0100

    Add pagination for users

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -14,14 +14,18 @@ import useUserDelete from "../../queries/users/delete";
 import {ModalType} from "../../types/table/modalType";
 import UserDetails from "../../components/Modal/Views/UserDetails";
 import {Grid} from "react-loader-spinner";
+import usePagination from "../../hooks/usePagination";
 
 function Users() {
     const columnHelper = createColumnHelper<UserFullData>()
+    const [paginationParams, setPaginationParams] = usePagination()
     const [modalOpen, setModalOpen] = useState<{isOpen: boolean, modalType: ModalType}>({isOpen: false, modalType: ModalType.none});
     const [itemId, setItemId] = useState<string>("");
     const userData = useAuthStore((state) => state.userData);
     const {isLoading, data, isSuccess} = useUserListQuery({
-        token: userData.token
+        token: userData.token,
+        pageIndex: paginationParams.pageIndex,
+        pageSize: paginationParams.pageSize
     })
 
     const columns = [
@@ -62,15 +66,20 @@ function Users() {
                 setModalOpen({isOpen: false, modalType: ModalType.none})
             }})
     }
-
-
-    const {users} = useMemo(() => ({
-        users: isSuccess ? data!.users : []
+    const {users, pageCount} = useMemo(() => ({
+        users: isSuccess ? data!.users : [],
+        pageCount: isSuccess ? data!.paginationInfo.lastPage : 0
     }), [data, isSuccess])
     const table = useReactTable({
         data: users,
         columns,
-        getCoreRowModel: getCoreRowModel()
+        getCoreRowModel: getCoreRowModel(),
+        manualPagination: true,
+        pageCount,
+        onPaginationChange: setPaginationParams,
+        state: {
+            pagination: paginationParams
+        }
     })
     const polishTableName = useTableListing(TableListingType.users);
 
@@ -81,6 +90,12 @@ function Users() {
                    rows={table.getRowModel().rows}
                    isLoading={isLoading}
                    tableName={polishTableName}
+                   tablePaginationState={table.getState().pagination}
+                   previousPage={table.previousPage}
+                   pageCount={table.getPageCount()}
+                   hasPreviousPage={table.getCanPreviousPage()}
+                   nextPage={table.nextPage}
+                   hasNextPage={table.getCanNextPage()}
              tableListing={TableListingType.users}/>
             {modalOpen.isOpen && modalOpen.modalType === ModalType.delete && <Modal><ViewDelete id={itemId} closeModal={setModalOpen} deleteMutation={deleteUser}/></Modal>}
             {modalOpen.isOpen && modalOpen.modalType === ModalType.details && <Modal><UserDetails id={itemId} token={userData.token} closeModal={setModalOpen}/></Modal>}
